67d55f94717ba009ace7fee5a1fbbe575f637492
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-12-12 06:37:46 +0500

    I have create a form in formik for bookmarking app.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
+import { Formik, Form, Field } from "formik";
 import { Grid } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
 import "./style.css";
@@ -26,24 +27,66 @@ export default function Home() {
   const [addBookmark] = useMutation(AddBookMarkMutation);
   let textfield, desc;
 
-  const addBookmarkSubmit = () => {
-    addBookmark({
-      variables: {
-        url: textfield.value,
-        desc: desc.value
-      },
-      refetchQueries: [{query:BookMarksQuery}],
-    })
-    console.log('textfield ===>', textfield.value);
-    console.log('Description  ===>', desc.value);
-  }
+  // const addBookmarkSubmit = () => {
+  //   addBookmark({
+  //     variables: {
+  //       url: textfield.value,
+  //       desc: desc.value
+  //     },
+  //     refetchQueries: [{query:BookMarksQuery}],
+  //   })
+  //   console.log('textfield ===>', textfield.value);
+  //   console.log('Description  ===>', desc.value);
+  // }
 
   return (
     <div> 
       <div>
-        <input type='text' placeholder="Enter bookmark URL" ref={node => textfield=node} />
+        {/* <input type='text' placeholder="Enter bookmark URL" ref={node => textfield=node} />
         <input type="text" placeholder="Description " ref={node => desc=node}/>
-        <button onClick={addBookmarkSubmit}> Add Bookmrk </button>
+        <button onClick={addBookmarkSubmit}> Add Bookmrk </button> */}
+
+       <Formik
+         onSubmit={(value, actions) => {
+          addBookmark({
+            variables: {
+              url: value.url,
+              desc: value.desc,
+             },
+               refetchQueries: [{query:BookMarksQuery}],
+           })
+
+          actions.resetForm({
+            values: {
+              url: "",
+              desc: "",              
+            },
+          })
+         }}
+
+         initialValues={{
+          url: "",
+          desc: "",          
+        }}
+       >
+        {formik => (
+          <Form onSubmit={formik.handleSubmit}>
+            <div className="input-main">
+              <div className="input-div">
+              <Field type="text" name="url" id="url" placeholder="link" />                 
+              <Field
+                type="description"
+                name="desc"
+                id="desc"
+                placeholder="description"
+              />
+              
+                <button type="submit"> Add Bookmark </button>
+              </div>
+            </div>
+          </Form>
+        )}
+       </Formik>   
       </div>
       {/* <p> { JSON.stringify(data) } </p>  */}
 
